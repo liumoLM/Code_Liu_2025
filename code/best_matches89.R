1735ca3027611f8430fb613977cd4c3f6a323b0a
Steven G. Rozen <steverozen@pm.me>
2025-12-29 22:18:18 -0500

    working multiway comparision for eventual plotting

--- code/best_matches89.R
+++ code/best_matches89.R
@@ -1,6 +1,6 @@
 library(gridExtra)
+library(mSigPlot)
 
-source("code/Koh89_Koh476_Plotting_Functions.R")
 source("code/best_matches.R")
 
 #' Run best_matches for ID89 signatures
@@ -14,7 +14,7 @@ best_matches89 <- function(out_dir, ...) {
 
   # Define plotit function using PlotKoh89Catalog directly
   plotit <- function(vec, title) {
-    PlotKoh89Catalog(vec, plot_title = title)
+    plot_89(vec, plot_title = title)
   }
 
   best_matches(
@@ -50,120 +50,10 @@ write.csv(
   row.names = FALSE
 )
 
-IndelType = c(
-  # Single base deletions - C
-  "[Del(C):R1]A",
-  "[Del(C):R1]T",
-  "[Del(C):R2]A",
-  "[Del(C):R2]T",
-  "[Del(C):R3]A",
-  "[Del(C):R3]T",
-  "[Del(C):R(4,5)]A",
-  "[Del(C):R(4,5)]T",
-  "[Del(C):R(1,5)]G",
-  "Del(C):R(6,9)",
-  # Single base deletions - T
-  "A[Del(T):R(1,4)]A",
-  "A[Del(T):R(1,4)]C",
-  "A[Del(T):R(1,4)]G",
-  "C[Del(T):R(1,4)]A",
-  "C[Del(T):R(1,4)]C",
-  "C[Del(T):R(1,4)]G",
-  "G[Del(T):R(1,4)]A",
-  "G[Del(T):R(1,4)]C",
-  "G[Del(T):R(1,4)]G",
-  "A[Del(T):R(5,7)]A",
-  "A[Del(T):R(5,7)]C",
-  "A[Del(T):R(5,7)]G",
-  "C[Del(T):R(5,7)]A",
-  "C[Del(T):R(5,7)]C",
-  "C[Del(T):R(5,7)]G",
-  "G[Del(T):R(5,7)]A",
-  "G[Del(T):R(5,7)]C",
-  "G[Del(T):R(5,7)]G",
-  "A[Del(T):R(8,)]A",
-  "A[Del(T):R(8,)]C",
-  "A[Del(T):R(8,)]G",
-  "C[Del(T):R(8,)]A",
-  "C[Del(T):R(8,)]C",
-  "C[Del(T):R(8,)]G",
-  "G[Del(T):R(8,)]A",
-  "G[Del(T):R(8,)]C",
-  "G[Del(T):R(8,)]G",
-  # Single base insertions - C
-  "A[Ins(C):R0]A",
-  "A[Ins(C):R0]T",
-  "Ins(C):R(0,3)",
-  "Ins(C):R(4,6)",
-  "Ins(C):R(7,)",
-  # Single base insertions - T
-  "A[Ins(T):R(0,4)]A",
-  "A[Ins(T):R(0,4)]C",
-  "A[Ins(T):R(0,4)]G",
-  "C[Ins(T):R(0,4)]A",
-  "C[Ins(T):R(0,4)]C",
-  "C[Ins(T):R(0,4)]G",
-  "G[Ins(T):R(0,4)]A",
-  "G[Ins(T):R(0,4)]C",
-  "G[Ins(T):R(0,4)]G",
-  "A[Ins(T):R(5,7)]A",
-  "A[Ins(T):R(5,7)]C",
-  "A[Ins(T):R(5,7)]G",
-  "C[Ins(T):R(5,7)]A",
-  "C[Ins(T):R(5,7)]C",
-  "C[Ins(T):R(5,7)]G",
-  "G[Ins(T):R(5,7)]A",
-  "G[Ins(T):R(5,7)]C",
-  "G[Ins(T):R(5,7)]G",
-  "A[Ins(T):R(8,)]A",
-  "A[Ins(T):R(8,)]C",
-  "A[Ins(T):R(8,)]G",
-  "C[Ins(T):R(8,)]A",
-  "C[Ins(T):R(8,)]C",
-  "C[Ins(T):R(8,)]G",
-  "G[Ins(T):R(8,)]A",
-  "G[Ins(T):R(8,)]C",
-  "G[Ins(T):R(8,)]G",
-  # Longer deletions (no MH)
-  "Del(2,4):R1",
-  "Del(5,):R1",
-  "Del(2,8):U(1,2):R(2,4)",
-  "Del(2,):U(1,2):R(5,)",
-  "Del(3,):U(3,):R2",
-  "Del(3,):U(3,):R(3,)",
-  # Longer insertions
-  "Ins(2,4):R0",
-  "Ins(5,):R0",
-  "Ins(2,4):R1",
-  "Ins(5,):R1",
-  "Ins(2,):R(2,4)",
-  "Ins(2,):R(5,)",
-  # Deletions with MH
-  "Del(2,5):M1",
-  "Del(3,5):M2",
-  "Del(4,5):M(3,4)",
-  "Del(6,):M1",
-  "Del(6,):M2",
-  "Del(6,):M3",
-  "Del(6,):M(4,)",
-  # Complex
-  "Complex"
+us_v_spectra89 = best_matches89(
+  "data/us_v_spectra89.tsv",
+  "data/type89_liu_et_al_sigs.tsv",
+  "data/type89_spectra.tsv"
 )
 
-if (FALSE) {
-  # This code updated the rownames from 89-type koh sig data
-  yy = NULL # Was the old table
-  uu = rownames(yy)
-  aa = gsub(
-    "R\\(5,9",
-    "R\\(5,",
-    gsub("R\\(3,9", "R\\(3,", gsub("R\\(8,9", "R\\(8,", uu))
-  )
-  bb = gsub("R\\(7,9", "R\\(7,", aa)
-  rownames(yy) = bb
-  dim(yy)
-  yy2 = yy[IndelType, ]
-  write.table(yy2, "data/type89_koh_sigs2.tsv", sep = '\t')
-  zz = read.delim("data/type89_our_sigs.tsv")
-  rownames(zz) == IndelType
-}
+write.table(us_v_spectra89, file = "data/us_v_spectra_80.tsv", sep = '\t')
