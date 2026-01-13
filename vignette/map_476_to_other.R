fb88552d7fe2e25f957ed542c10ae47c702f6f2b
Steven G. Rozen <steverozen@pm.me>
2026-01-13 11:01:54 -0500

    476 to 83 conversion works and is tested; cosine similarity calculation not connected; would need to be connected another way

--- vignette/map_476_to_other.R
+++ vignette/map_476_to_other.R
@@ -33,3 +33,54 @@ t476_to_89 <- function(t476) {
 
   new89
 }
+
+t476_to_83 <- function(t476) {
+  mut_type_mapping <- data.table::fread("./ID476_ID89_mapping.txt")
+  tmp <- read.delim(
+    "../Manuscript_data/Liu_et_al_final_83_type_signatures.tsv",
+    sep = '\t'
+  )
+  correct_row_order = tmp[, 1]
+  rm(tmp)
+
+  t476$mut83_class <- mut_type_mapping$indel83.class[match(
+    row.names(t476),
+    mut_type_mapping$indel476.class
+  )]
+
+  stopifnot(!is.na(t476$mut83_class))
+
+  new83 <- t476 %>%
+    group_by(mut83_class) %>%
+    summarise(across(
+      where(is.numeric),
+      ~ sum(.x, na.rm = TRUE),
+      .names = "{.col}_converted"
+    )) %>%
+    filter(mut83_class != "unmapped") %>%
+    as.data.frame()
+
+  row.names(new83) <- new83$mut83_class
+  for (rname in c(
+    "DEL:repeats:2:5+",
+    "DEL:repeats:3:5+",
+    "DEL:repeats:4:4",
+    "INS:repeats:5+:2",
+    "INS:repeats:5+:5+"
+  )) {
+    if (rname %in% rownames(new83)) {
+      stop("Programming error")
+    }
+    new_row <- data.frame(matrix(0, nrow = 1, ncol = ncol(new83)))
+    colnames(new_row) <- colnames(new83)
+    rownames(new_row) <- rname
+    new83 <- rbind(new83, new_row)
+  }
+
+  stopifnot(length(symdiff(correct_row_order, rownames(new83))) == 0)
+
+  new83 <- new83[, -1, drop = FALSE]
+  new83 = new83[correct_row_order, ]
+
+  new83
+}
