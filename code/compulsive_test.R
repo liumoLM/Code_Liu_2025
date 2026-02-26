20f6b756c21f1542064b0cd1a444018b22bfb7ed
Steven G. Rozen <steverozen@pm.me>
2026-02-26 10:09:19 -0500

    issue with difference to clipped spectra solved -- didn't filter to PASS

--- code/compulsive_test.R
+++ code/compulsive_test.R
@@ -3,13 +3,20 @@ source("code/read_annotated_vcf.R")
 vcf <- read_annotated_vcf("X::DRUP01050028T")
 
 # Way #1: Use clip_le_9 = TRUE (ICAMS handles the clipping internally)
-cat1 <- ICAMS::annot_vcf_to_476_catalog(vcf, sample_id = "clip_le_9_TRUE",
-                                         clip_le_9 = TRUE)
+cat1 <- ICAMS::annot_vcf_to_476_catalog(
+  vcf,
+  sample_id = "clip_le_9_TRUE",
+  clip_le_9 = TRUE
+)
 
 # Way #2: Pre-filter VCF to R <= 9, then use clip_le_9 = FALSE
 vcf_filtered <- vcf[R <= 9, ]
-cat2 <- ICAMS::annot_vcf_to_476_catalog(vcf_filtered, sample_id = "prefiltered_R_le_9",
-                                         clip_le_9 = FALSE)
+cat2 <- ICAMS::annot_vcf_to_476_catalog(
+  vcf_filtered,
+  sample_id = "prefiltered_R_le_9",
+  clip_le_9 = FALSE,
+  FILTER_PASS = TRUE
+)
 
 # Extract as numeric vectors
 v1 <- cat1[, 1]
@@ -40,8 +47,10 @@ if (length(diff_idx) > 0) {
 
 # Compare to the published spectra file
 cat("\n--- Comparison to 476_clipped_spectra.tsv ---\n")
-spectra <- read.delim("Manuscript_data/476_clipped_spectra.tsv",
-                      check.names = FALSE)
+spectra <- read.delim(
+  "Manuscript_data/476_clipped_spectra.tsv",
+  check.names = FALSE
+)
 v_pub <- spectra[, "DRUP01050028T"]
 
 cos_pub <- lsa::cosine(v1, v_pub)
