88cb81d4d7036614138d12e7fa23889024a896f5
Steven G. Rozen <steverozen@pm.me>
2026-03-26 20:04:38 -0400

    upated connection table

--- Manuscript_data/Mo_CAP9_analysis/make_connection_table.R
+++ Manuscript_data/Mo_CAP9_analysis/make_connection_table.R
@@ -5,13 +5,13 @@ source(here::here("code", "find_best_match_spectra.R"))
 source(here::here("vignette", "vhelpers.R"))
 
 # Read signature files (mutation types as rows, signatures as columns)
-sigs_83  <- as.matrix(read_finalized("83_signatures"))
-sigs_89  <- as.matrix(read_finalized("89_signatures"))
+sigs_83 <- as.matrix(read_finalized("83_signatures"))
+sigs_89 <- as.matrix(read_finalized("89_signatures"))
 sigs_476 <- as.matrix(read_finalized("476_signatures"))
 
 # Spectra file paths
-spectra_83_path  <- finalized_files[["83_spectra"]]
-spectra_89_path  <- finalized_files[["89_spectra"]]
+spectra_83_path <- finalized_files[["83_spectra"]]
+spectra_89_path <- finalized_files[["89_spectra"]]
 spectra_476_path <- finalized_files[["476_spectra"]]
 
 # Map an 89/476-type signature name to the corresponding 83-type name.
@@ -21,7 +21,10 @@ spectra_476_path <- finalized_files[["476_spectra"]]
 map_89_to_83 <- function(name89) {
   m <- regmatches(name89, regexpr("^InsDel(\\d+)[a-z]?$", name89, perl = TRUE))
   if (length(m) > 0) {
-    return(paste0("C_ID", sub("^InsDel(\\d+)[a-z]?$", "\\1", name89, perl = TRUE)))
+    return(paste0(
+      "C_ID",
+      sub("^InsDel(\\d+)[a-z]?$", "\\1", name89, perl = TRUE)
+    ))
   }
   m <- regmatches(name89, regexpr("^InsDel_([A-Z])$", name89, perl = TRUE))
   if (length(m) > 0) {
@@ -34,7 +37,12 @@ map_89_to_83 <- function(name89) {
   if (length(m) > 0) {
     return(paste0(
       "ID_",
-      sub("^InsDel_([A-Z])_(alpha|beta|gamma|delta)$", "\\1", name89, perl = TRUE)
+      sub(
+        "^InsDel_([A-Z])_(alpha|beta|gamma|delta)$",
+        "\\1",
+        name89,
+        perl = TRUE
+      )
     ))
   }
   stop(paste("Cannot map 89-type name to 83-type:", name89))
@@ -44,8 +52,9 @@ sig89_names <- colnames(sigs_89)
 sig83_names <- sapply(sig89_names, map_89_to_83, USE.NAMES = FALSE)
 
 stopifnot(
-  "Some mapped 83-type names are not present in liu_et_al_83_signatures.tsv" =
-    all(sig83_names %in% colnames(sigs_83))
+  "Some mapped 83-type names are not present in liu_et_al_83_signatures.tsv" = all(
+    sig83_names %in% colnames(sigs_83)
+  )
 )
 
 # For each 89/476-type signature, find the top 3 best-matching samples in
@@ -58,40 +67,47 @@ build_row <- function(idx) {
   name89 <- sig89_names[idx]
   name83 <- sig83_names[idx]
 
-  top83  <- find_top_n_match_names(
-    sigs_83[, name83, drop = FALSE], spectra_83_path,
-    n = n, min_mutations = min_mutations
+  top83 <- find_top_n_match_names(
+    sigs_83[, name83, drop = FALSE],
+    spectra_83_path,
+    n = n,
+    min_mutations = min_mutations
   )
-  top89  <- find_top_n_match_names(
-    sigs_89[, name89, drop = FALSE], spectra_89_path,
-    n = n, min_mutations = min_mutations
+  # browser()
+  top89 <- find_top_n_match_names(
+    sigs_89[, name89, drop = FALSE],
+    spectra_89_path,
+    n = n,
+    min_mutations = min_mutations
   )
   top476 <- find_top_n_match_names(
-    sigs_476[, name89, drop = FALSE], spectra_476_path,
-    n = n, min_mutations = min_mutations
+    sigs_476[, name89, drop = FALSE],
+    spectra_476_path,
+    n = n,
+    min_mutations = min_mutations
   )
 
   data.frame(
-    InDel89        = name89,
-    InDel83        = name83,
-    BestMatch83_1  = top83$names[1, 1],
-    CosSim83_1     = top83$cosines[1, 1],
-    BestMatch83_2  = top83$names[1, 2],
-    CosSim83_2     = top83$cosines[1, 2],
-    BestMatch83_3  = top83$names[1, 3],
-    CosSim83_3     = top83$cosines[1, 3],
-    BestMatch89_1  = top89$names[1, 1],
-    CosSim89_1     = top89$cosines[1, 1],
-    BestMatch89_2  = top89$names[1, 2],
-    CosSim89_2     = top89$cosines[1, 2],
-    BestMatch89_3  = top89$names[1, 3],
-    CosSim89_3     = top89$cosines[1, 3],
+    InDel89 = name89,
+    InDel83 = name83,
+    BestMatch83_1 = top83$names[1, 1],
+    CosSim83_1 = top83$cosines[1, 1],
+    BestMatch83_2 = top83$names[1, 2],
+    CosSim83_2 = top83$cosines[1, 2],
+    BestMatch83_3 = top83$names[1, 3],
+    CosSim83_3 = top83$cosines[1, 3],
+    BestMatch89_1 = top89$names[1, 1],
+    CosSim89_1 = top89$cosines[1, 1],
+    BestMatch89_2 = top89$names[1, 2],
+    CosSim89_2 = top89$cosines[1, 2],
+    BestMatch89_3 = top89$names[1, 3],
+    CosSim89_3 = top89$cosines[1, 3],
     BestMatch476_1 = top476$names[1, 1],
-    CosSim476_1    = top476$cosines[1, 1],
+    CosSim476_1 = top476$cosines[1, 1],
     BestMatch476_2 = top476$names[1, 2],
-    CosSim476_2    = top476$cosines[1, 2],
+    CosSim476_2 = top476$cosines[1, 2],
     BestMatch476_3 = top476$names[1, 3],
-    CosSim476_3    = top476$cosines[1, 3],
+    CosSim476_3 = top476$cosines[1, 3],
     stringsAsFactors = FALSE
   )
 }
