ba050e0eec402f581da7813c8586099c77caf349
Steven G. Rozen <steverozen@pm.me>
2026-05-10 17:12:11 -0400

    reviewed total spectra autogenerated table 1

--- code_for_internal_exploration/total_spectra.R
+++ code_for_internal_exploration/total_spectra.R
@@ -10,15 +10,25 @@ library(gridExtra)
 types <- c("83", "89", "476")
 
 plot_fns <- list(
-  "83"  = mSigPlot::plot_ID83,
-  "89"  = mSigPlot::plot_ID89,
+  "83" = mSigPlot::plot_ID83,
+  "89" = mSigPlot::plot_ID89,
   "476" = mSigPlot::plot_ID476
 )
 
 plot_args <- list(
-  "83"  = list(block_label_cex = 0.7, axis_text_x_cex = 0.5, axis_title_y_cex = 0.1),
-  "89"  = list(block_label_cex = 1.5, axis_text_x_cex = 0.4, axis_title_y_cex = 0.1),
-  "476" = list()
+  "83" = list(
+    block_label_cex = 0.7,
+    axis_text_x_cex = 0.5,
+    axis_title_y_cex = 0.1,
+    count_label_cex = 0.5
+  ),
+  "89" = list(
+    block_label_cex = 1.5,
+    axis_text_x_cex = 0.4,
+    axis_title_y_cex = 0.1,
+    count_label_cex = 0.7
+  ),
+  "476" = list(count_label_cex = 0.7)
 )
 
 plots <- lapply(types, function(type) {
@@ -27,13 +37,19 @@ plots <- lapply(types, function(type) {
     paste0("liu_et_al_", type, "_spectra.tsv")
   )
   spec <- read.delim(spec_file, sep = "\t", row.names = 1, check.names = FALSE)
-  total <- data.frame(Total = rowSums(spec), row.names = rownames(spec),
-                      check.names = FALSE)
+  total <- data.frame(
+    Total = rowSums(spec),
+    row.names = rownames(spec),
+    check.names = FALSE
+  )
+  if (type == "476") {
+    rownames(total) <- NULL
+  }
   do.call(plot_fns[[type]], c(list(total), plot_args[[type]]))
 })
 
 out_path <- here::here("Manuscript_data/finalized_cap9", "total_spectra.pdf")
-pdf(out_path, width = 8.5, height = 11)
+cairo_pdf(out_path, width = 8.5, height = 11)
 grid.arrange(grobs = plots, ncol = 1, nrow = 3)
 dev.off()
 message("Wrote ", out_path)
